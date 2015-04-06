4d8cf3633ca37e45884f9107c32bc1b07ec2d4f6
Phil Derksen <pderksen@gmail.com>
2015-04-06 12:58:53 -0700

    Experimenting with admin notice dismissible button introduced in WP 4.2.

--- stripe/admin/views/admin-install-notice.php
+++ stripe/admin/views/admin-install-notice.php
@@ -21,10 +21,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	}
 </style>
 
-<div id="sc-install-notice" class="updated">
+<div id="sc-install-notice" class="updated notice is-dismissible">
 	<p>
 		<?php echo $this->get_plugin_title() . __( ' is now installed.', 'sc' ); ?>
 		<a href="<?php echo add_query_arg( 'page', $this->plugin_slug, admin_url( 'admin.php' ) ); ?>" class="button-primary"><?php _e( 'Get started by entering your Stripe keys', 'sc' ); ?></a>
 		<a href="<?php echo add_query_arg( 'sc-dismiss-install-nag', 1 ); ?>" class="button-secondary"><?php _e( 'Hide this', 'sc' ); ?></a>
 	</p>
+	<button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button>
 </div>
